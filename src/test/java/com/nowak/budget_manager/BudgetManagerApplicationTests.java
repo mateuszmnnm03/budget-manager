625499806294548ad6324277b9fd7f6506b1be6b
Mateusz <mateusz.nnowak2003@gmail.com>
2026-06-03 17:47:07 +0200

    Initial commit with dockerfile and Spring Boot structure

---
+++ src/test/java/com/nowak/budget_manager/BudgetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowak.budget_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
